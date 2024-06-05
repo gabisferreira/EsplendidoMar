5cac86f02bc39637dc9d2d8dc8b4353553a415e8
gabisferreira <gabriela.ferreira@sptech.school>
2024-06-05 01:49:35 -0300

    Ultimas atualizações

--- ExplendidoMarScript.sql
+++ ExplendidoMarScript.sql
@@ -9,7 +9,7 @@ email varchar(45) unique,
 senha varchar(45),
 constraint checkEmail check (email like ('%@%'))) ;
 
-INSERT INTO usuario (nome, email, senha) VALUES
+INSERT INTO Usuario (nome, email, senha) VALUES
 ('ana', 'ana@gmail', '123'),
 ('rafa', 'rafa@gmail', '765'),
 ('kaio', 'kaio@gmail', '987'),
@@ -38,40 +38,26 @@ fkQuiz int,
 Pontuacao int,
 primary key (fkUsuario, fkQuiz, idPontuacao),
 constraint fkUsuarioQuiz foreign key (fkUsuario) references Usuario (idUsuario),
-constraint fkQuizUsuario foreign key (fkQuiz) references Quiz (idQuiz))
-;
+constraint fkQuizUsuario foreign key (fkQuiz) references Quiz (idQuiz));
+
 select * from PontuacaoQuiz;
 
-
-Insert into PontuaçãoQuiz values
-(1, 1, 8),
-(2, 1, 9),
-(3, 1, 3),
-(4, 1, 6),
-(5, 1, 7),
-(6, 1, 10),
-(7, 1, 5),
-(8, 1, 2);
-
-insert into pontuacaoquiz values (3, 1, 1, 10);
-
+Insert into PontuacaoQuiz values
+(1, 1, 1, 8);
 
 Select idPontuacao from PontuacaoQuiz where fkusuario = 1 and fkquiz = 1 order by idpontuacao desc limit 1;
 
-update pontuaçãoquiz set pontuação = 1 where fkusuario = 1;
+describe PontuacaoQuiz;
 
-describe pontuaçãoquiz;
-
-Select *from PontuaçãoQuiz where fkUsuario = 1;
+Select *from PontuacaoQuiz where fkUsuario = 1;
 
 Select 	Usuario.nome as NomeUsuario, 
-		PontuaçãoQuiz.Pontuação as Pontuação
-		from Usuario join PontuaçãoQuiz
-		on idUsuario = PontuaçãoQuiz.fkUsuario;
+		PontuacaoQuiz.Pontuacao as Pontuacao
+		from Usuario join PontuacaoQuiz
+		on idUsuario = PontuacaoQuiz.fkUsuario;
         
 SELECT idUsuario,
 		nome,
 		email, 
 		PontuacaoQuiz.Pontuacao as 'PontuacaoQuiz' 
-        FROM usuario join PontuacaoQuiz where idUsuario = 1;
-
+        FROM Usuario join PontuacaoQuiz where idUsuario = 1;
